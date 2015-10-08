a20eee12f82a91423dcd11b1d052d0896a386c46
mgbolduc <mgbolduc3@gmail.com>
2015-10-08 12:42:20 -0400

    Commit 12:42

--- Scanner/app/src/main/java/com/app/squad/scanner/MainActivity.java
+++ Scanner/app/src/main/java/com/app/squad/scanner/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,57 +15,26 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private String codeFormat,codeContent;
-    private TextView formatTxt, contentTxt;
+
+    Button bLogin;
+    UserLocalStorage userLocalStore;
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
-    }
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
 
-    /**
-     * event handler for scan button
-     * @param view view of the activity
-     */
-    public void scanNow(View view){
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
-        integrator.setPrompt("Scan a barcode");
-        integrator.setResultDisplayDuration(0);
-        integrator.setWide();  // Wide scanning rectangle, may work better for 1D barcodes
-        integrator.setCameraId(0);  // Use a specific camera of the device
-        integrator.initiateScan();
-    }
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bLogin.setOnClickListener(this);
 
-    /**
-     * function handle scan result
-     * @param requestCode
-     * @param resultCode
-     * @param intent
-     */
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-//retrieve scan result
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        userLocalStore = new UserLocalStorage(this);
 
-        if (scanningResult != null) {
-//we have a result
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-
-// display it on screen
-            formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
-
-        }else{
-            Toast toast = Toast.makeText(getApplicationContext(),"No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
     }
 
     @Override
@@ -86,5 +57,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bLogin:
+
+                startActivity(new Intent(this, Scan.class));
+
+                break;
+
+        }
     }
 }
